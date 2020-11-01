e421cafbb933ae650ef1ac24a9cd3e1da1d9ae10
GovindMehra <66516101+GovindMehra@users.noreply.github.com>
2020-11-01 15:31:17 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# My---Recipes
+# staticSquare
+A static square made using Matter.js physics engine
---
+++ sketch.js
@@ -0,0 +1,15 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+function setup(){
+createCanvas(400,100);
+}
+
+function draw(){
+    background("black");
+    textSize(50);
+    fill("red");
+    text("My Recipes",70,65);
+   }
+
